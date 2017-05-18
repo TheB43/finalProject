4da1af55b6ebf4dc6979af99c967ec7133e3929d
Jeremy Pasco <jeremy.pasco@gmail.com>
2017-05-18 10:40:46 -0700

    Add files via upload

--- Casino/IntroScreen.java
+++ Casino/IntroScreen.java
@@ -16,6 +16,16 @@ public class IntroScreen extends World
     public IntroScreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(900, 550, 1); 
+        getBackground().drawImage(new GreenfootImage("Casino-introscreen.png"), 0, 0);
+        Greenfoot.start();
+    }
+    public void act() {
+        MouseInfo mouse = Greenfoot.getMouseInfo();
+        int x = mouse.getX();
+        int y = mouse.getY();
+        if(Greenfoot.mouseClicked(null)) {
+            
+        }
     }
 }
